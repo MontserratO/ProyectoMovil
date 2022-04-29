259f966ef63e804bb6ce3c464dc625e39f997c13
DiegoDiazDeLeon <diegodiazdeleoncai@gmail.com>
2022-04-29 01:29:31 -0500

    add commit prueba

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'dart:developer';
 
 class Product {
   const Product({required this.imUr, required this.name, required this.cuan});
